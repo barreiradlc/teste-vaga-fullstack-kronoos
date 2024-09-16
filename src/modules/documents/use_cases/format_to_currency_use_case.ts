87fc472d1645579492051f65b5786ee451322312
Augusto Barreira <barreira266@hotmail.com>
2024-09-16 02:27:55 -0300

    Correção em implementação de valor monetário

--- src/modules/documents/use_cases/format_to_currency_use_case.ts
+++ src/modules/documents/use_cases/format_to_currency_use_case.ts
@@ -1,13 +1,11 @@
 class FormatToCurrencyUseCase {
-
-  async execute(value: number, locale = 'pt-BR', currency = 'BRL') {
+  execute(value: number | string, locale = 'pt-BR', currency = 'BRL') {
     return new Intl.NumberFormat(locale, {
       style: 'currency',
       currency: currency,
       minimumFractionDigits: 2,
       maximumFractionDigits: 2,
-    }).format(value);
-    // return value;
+    }).format(Number(value));
   }
 
 }
