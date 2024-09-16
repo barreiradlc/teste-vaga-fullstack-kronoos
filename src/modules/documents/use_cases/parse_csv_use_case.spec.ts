4633b4e79c5aa596963b78e994aac49ae97178f6
Augusto Barreira <barreira266@hotmail.com>
2024-09-16 02:29:13 -0300

    Correção em caso de uso de leitura de planilha

--- src/modules/documents/use_cases/parse_csv_use_case.spec.ts
+++ src/modules/documents/use_cases/parse_csv_use_case.spec.ts
@@ -1,5 +1,6 @@
-import { beforeAll, describe, expect, it } from 'vitest'
-import { ParseCsvUseCase } from './parse_csv_use_case'
+import { Item } from '@prisma/client';
+import { beforeAll, describe, expect, it } from 'vitest';
+import { ParseCsvUseCase } from './parse_csv_use_case';
 
 let sut: ParseCsvUseCase
 
@@ -9,9 +10,20 @@ describe("ParseCsvUseCase", () => {
   })
 
   it('Should be able to parse CSV file', async () => {
-    let path = 'data.sample.csv'
-    let result = await sut.execute(path)
+    let path = 'assets/data.csv'
+
+    let result: Item[] = await sut.execute(path)    
+
     expect(result).toBeInstanceOf(Array)
   })
 
+  it('Should NOT be able to parse an invalid CSV file', async () => {
+    let path = 'assets/data.invalid.txt'
+
+    await expect(() =>
+      sut.execute(path),
+    )
+      .rejects
+      .toBeInstanceOf(Error)
+  })
 })
--- src/modules/documents/use_cases/parse_csv_use_case.ts
+++ src/modules/documents/use_cases/parse_csv_use_case.ts
@@ -1,11 +1,30 @@
+import { Item } from '@prisma/client';
 import csv from 'csv-parser';
 import fs from 'fs';
+
+const SPREAD_SHEET_VALID_FORMATS = [
+  'csv',
+  'xls',
+  'xlsx',
+  'xlsm',
+  'xlsb',
+  'xltx',
+  'xlt',
+  'ods'
+]
 
 class ParseCsvUseCase {
 
   async execute(filePath: string) {
     try {
-      return new Promise((resolve, reject) => {
+      return new Promise<Item[]>((resolve, reject) => {
+        const fileExtension = filePath.split('.').pop()
+        const isAValidFile = SPREAD_SHEET_VALID_FORMATS.some((file) => file.includes(`${fileExtension}`))
+
+        if (!isAValidFile) {
+          throw new Error("Invalid format type");
+        }
+
         const results: any = [];
 
         fs.createReadStream(filePath)
@@ -15,8 +34,8 @@ class ParseCsvUseCase {
           .on('error', (error) => reject(error));
       });
 
-    } catch (error) {
-      console.error(error)
+    } catch (error) {      
+      throw new Error(`${error}`);      
     }
   }
 }
